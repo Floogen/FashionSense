130c6841c9ba35c3965c0e9c69a88d019c3305e0
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-10-23 17:03:36 -0500

    Implemented AnimationModel.Type and refactored RequiredElapsedMovementSeconds to RequiredElapsedMovementMilliseconds

--- LivelyHair/Framework/Models/AnimationModel.cs
+++ LivelyHair/Framework/Models/AnimationModel.cs
@@ -8,9 +8,16 @@ namespace LivelyHair.Framework.Models
 {
     public class AnimationModel
     {
+        internal enum Type
+        {
+            Unknown,
+            Idle,
+            Moving
+        }
+
         public int Frame { get; set; }
         public int RequiredMovementSpeed { get; set; } = -1;
-        public int RequiredElapsedMovementSeconds { get; set; } = -1;
+        public int RequiredElapsedMovementMilliseconds { get; set; } = -1;
         public int Duration { get; set; } = 1000;
     }
 }
