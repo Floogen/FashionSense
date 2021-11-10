389cf6e5dde01899a19adc06f930bf8a386f7d23
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-11-10 16:24:31 -0600

    Implemented RandomRange for conditions

--- FashionSense/Framework/Models/Generic/Condition.cs
+++ FashionSense/Framework/Models/Generic/Condition.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FashionSense.Framework.Models.Generic.Random;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using StardewValley;
 
 namespace FashionSense.Framework.Models.Generic
 {
@@ -25,6 +29,18 @@ namespace FashionSense.Framework.Models.Generic
 
         internal T GetParsedValue<T>()
         {
+            if (Value is JObject modelContext)
+            {
+                if (modelContext["RandomRange"] != null)
+                {
+                    var randomRange = JsonConvert.DeserializeObject<RandomRange>(modelContext["RandomRange"].ToString());
+
+                    return (T)Convert.ChangeType(Game1.random.Next(randomRange.Min, randomRange.Max), typeof(T));
+                }
+
+                return (T)Value;
+            }
+
             return (T)Value;
         }
 
