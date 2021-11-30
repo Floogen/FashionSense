1c8e59aaa179e6b0916b99ac71beb8235923ebc9
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-11-30 17:20:43 -0600

    Adjusted shirt draw layer

--- FashionSense/Framework/Patches/Renderer/FarmerRendererPatch.cs
+++ FashionSense/Framework/Patches/Renderer/FarmerRendererPatch.cs
@@ -1213,7 +1213,7 @@ namespace FashionSense.Framework.Patches.Renderer
             }
             else
             {
-                float layerOffset = who.FacingDirection > 0 ? 1.5E-07f : -0.03E-05f;
+                float layerOffset = 1.5E-07f;
 
                 var shirtColor = new Color() { PackedValue = Game1.player.modData.ContainsKey(ModDataKeys.UI_HAND_MIRROR_SHIRT_COLOR) ? uint.Parse(Game1.player.modData[ModDataKeys.UI_HAND_MIRROR_SHIRT_COLOR]) : who.hairstyleColor.Value.PackedValue };
                 if (shirtModel.DisableGrayscale)
