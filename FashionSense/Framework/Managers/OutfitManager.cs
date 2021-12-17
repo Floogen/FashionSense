9569effb53b634c203ea7d5d0bcf9dff3c9cb2d2
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-12-16 20:04:47 -0600

    Handled caching of outfit colors

--- FashionSense/Framework/Managers/OutfitManager.cs
+++ FashionSense/Framework/Managers/OutfitManager.cs
@@ -1,6 +1,7 @@
 ﻿using FashionSense.Framework.Models;
 using FashionSense.Framework.Models.Hair;
 using FashionSense.Framework.Utilities;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewValley;
@@ -89,14 +90,23 @@ namespace FashionSense.Framework.Managers
 
         public void SetOutfit(Farmer who, Outfit outfit)
         {
+            who.modData[ModDataKeys.CUSTOM_HAIR_ID] = String.IsNullOrEmpty(outfit.HairId) ? "None" : outfit.HairId;
             who.modData[ModDataKeys.CUSTOM_ACCESSORY_ID] = String.IsNullOrEmpty(outfit.AccessoryOneId) ? "None" : outfit.AccessoryOneId;
             who.modData[ModDataKeys.CUSTOM_ACCESSORY_SECONDARY_ID] = String.IsNullOrEmpty(outfit.AccessoryTwoId) ? "None" : outfit.AccessoryOneId;
             who.modData[ModDataKeys.CUSTOM_ACCESSORY_TERTIARY_ID] = String.IsNullOrEmpty(outfit.AccessoryThreeId) ? "None" : outfit.AccessoryOneId;
-            who.modData[ModDataKeys.CUSTOM_HAIR_ID] = String.IsNullOrEmpty(outfit.HairId) ? "None" : outfit.HairId;
             who.modData[ModDataKeys.CUSTOM_HAT_ID] = String.IsNullOrEmpty(outfit.HatId) ? "None" : outfit.HatId;
             who.modData[ModDataKeys.CUSTOM_SHIRT_ID] = String.IsNullOrEmpty(outfit.ShirtId) ? "None" : outfit.ShirtId;
             who.modData[ModDataKeys.CUSTOM_SLEEVES_ID] = String.IsNullOrEmpty(outfit.SleevesId) ? "None" : outfit.SleevesId;
             who.modData[ModDataKeys.CUSTOM_PANTS_ID] = String.IsNullOrEmpty(outfit.PantsId) ? "None" : outfit.PantsId;
+
+            who.changeHairColor(new Color() { PackedValue = uint.Parse(outfit.HairColor) });
+            who.modData[ModDataKeys.UI_HAND_MIRROR_ACCESSORY_COLOR] = outfit.AccessoryOneColor;
+            who.modData[ModDataKeys.UI_HAND_MIRROR_ACCESSORY_SECONDARY_COLOR] = outfit.AccessoryTwoColor;
+            who.modData[ModDataKeys.UI_HAND_MIRROR_ACCESSORY_TERTIARY_COLOR] = outfit.AccessoryThreeColor;
+            who.modData[ModDataKeys.UI_HAND_MIRROR_HAT_COLOR] = outfit.HatColor;
+            who.modData[ModDataKeys.UI_HAND_MIRROR_SHIRT_COLOR] = outfit.ShirtColor;
+            who.modData[ModDataKeys.UI_HAND_MIRROR_SLEEVES_COLOR] = outfit.SleevesColor;
+            who.modData[ModDataKeys.UI_HAND_MIRROR_PANTS_COLOR] = outfit.PantsColor;
         }
 
         public void OverrideOutfit(Farmer who, string name)
--- FashionSense/Framework/Models/ModData/Outfit.cs
+++ FashionSense/Framework/Models/ModData/Outfit.cs
@@ -16,6 +16,8 @@ namespace FashionSense.Framework.Models
     public class Outfit
     {
         public string Name { get; set; }
+
+        // Ids
         public string AccessoryOneId { get; set; }
         public string AccessoryTwoId { get; set; }
         public string AccessoryThreeId { get; set; }
@@ -25,6 +27,16 @@ namespace FashionSense.Framework.Models
         public string SleevesId { get; set; }
         public string PantsId { get; set; }
 
+        // Colors
+        public string AccessoryOneColor { get; set; }
+        public string AccessoryTwoColor { get; set; }
+        public string AccessoryThreeColor { get; set; }
+        public string HairColor { get; set; }
+        public string HatColor { get; set; }
+        public string ShirtColor { get; set; }
+        public string SleevesColor { get; set; }
+        public string PantsColor { get; set; }
+
         public Outfit()
         {
 
@@ -33,14 +45,24 @@ namespace FashionSense.Framework.Models
         public Outfit(Farmer who, string name)
         {
             Name = name;
+
+            HairId = who.modData[ModDataKeys.CUSTOM_HAIR_ID];
             AccessoryOneId = who.modData[ModDataKeys.CUSTOM_ACCESSORY_ID];
             AccessoryTwoId = who.modData[ModDataKeys.CUSTOM_ACCESSORY_SECONDARY_ID];
             AccessoryThreeId = who.modData[ModDataKeys.CUSTOM_ACCESSORY_TERTIARY_ID];
-            HairId = who.modData[ModDataKeys.CUSTOM_HAIR_ID];
             HatId = who.modData[ModDataKeys.CUSTOM_HAT_ID];
             ShirtId = who.modData[ModDataKeys.CUSTOM_SHIRT_ID];
             SleevesId = who.modData[ModDataKeys.CUSTOM_SLEEVES_ID];
             PantsId = who.modData[ModDataKeys.CUSTOM_PANTS_ID];
+
+            HairColor = who.hairstyleColor.Value.PackedValue.ToString();
+            AccessoryOneColor = who.modData[ModDataKeys.UI_HAND_MIRROR_ACCESSORY_COLOR];
+            AccessoryTwoColor = who.modData[ModDataKeys.UI_HAND_MIRROR_ACCESSORY_SECONDARY_COLOR];
+            AccessoryThreeColor = who.modData[ModDataKeys.UI_HAND_MIRROR_ACCESSORY_TERTIARY_COLOR];
+            HatColor = who.modData[ModDataKeys.UI_HAND_MIRROR_HAT_COLOR];
+            ShirtColor = who.modData[ModDataKeys.UI_HAND_MIRROR_SHIRT_COLOR];
+            SleevesColor = who.modData[ModDataKeys.UI_HAND_MIRROR_SLEEVES_COLOR];
+            PantsColor = who.modData[ModDataKeys.UI_HAND_MIRROR_PANTS_COLOR];
         }
     }
 }
