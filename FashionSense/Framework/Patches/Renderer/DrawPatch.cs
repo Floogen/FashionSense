b053dddc86e5fd984f8998322fae649d3fa0c8a9
Floogen <31755155+Floogen@users.noreply.github.com>
2025-12-17 16:17:09 -0600

    Fixed performance issue when swimming with custom body
    Was due to FarmerRendererPatch.AreColorMasksPendingRefresh not being set to false while swimming.

--- FashionSense/Framework/Patches/Renderer/DrawPatch.cs
+++ FashionSense/Framework/Patches/Renderer/DrawPatch.cs
@@ -350,6 +350,8 @@ namespace FashionSense.Framework.Patches.Renderer
                 {
                     b.Draw(Game1.staminaRect, new Rectangle((int)position.X + (int)who.yOffset + 8, (int)position.Y - 128 + sourceRect.Height * 4 + (int)origin.Y - (int)who.yOffset, sourceRect.Width * 4 - (int)who.yOffset * 2 - 16, 4), Game1.staminaRect.Bounds, Color.White * 0.75f, 0f, Vector2.Zero, SpriteEffects.None, drawManager.LayerDepth + 0.001f);
                 }
+
+                FarmerRendererPatch.AreColorMasksPendingRefresh = false;
                 return;
             }
 
